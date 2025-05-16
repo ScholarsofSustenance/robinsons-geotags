b1ca3994d41a3ba876fca3b030a9c2fe80f61c13
ScholarsofSustenance <biancaelisealarcon18@gmail.com>
2025-05-16 10:39:00 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-{"type": "FeatureCollection", "features": [], "name": "script.js"}
+
+function loadRoute(area) {
+    const routeInfo = document.getElementById('route-info');
+    routeInfo.innerHTML = '<h3>' + area.toUpperCase() + ' Route Info</h3><p>Route data loaded for: ' + area + '</p>';
+}
